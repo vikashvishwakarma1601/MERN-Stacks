7864ef9bfa3a81bc09b0da2279ba221e75b46918
Vikash Vishwakarma <vishwakarma.vikash1601@gmail.com>
2020-04-03 13:12:06 +0530

    Update index.js

--- Todo App/index.js
+++ Todo App/index.js
@@ -27,6 +27,18 @@ class TodoService
         this.todos = this.todos.concat(newTodo)
         this.todoId++
     }
+
+    editTitle(Id,newTitle)
+    {
+        this.todos = this.todos.map(todo=>{
+            if(todo.id==Id)
+            {
+                todo.title = newTitle
+            }
+            return todo
+        })
+    }
+
     deleteTodo(Id)
     {
         this.todos = this.todos.filter(todo => todo.id!=Id)
@@ -36,7 +48,7 @@ class TodoService
         this.todos = this.todos.map(todo =>{
             if(todo.id == Id)
             {
-                todo.completed = true
+                todo.completed = !todo.completed
             }
             return todo
         })
@@ -60,19 +72,14 @@ class TodoService
     {
         if(flag=='All')
         {
-            // return this.todos.map(todo =>{
-            //     console.log(todo)
-            // })
             return this.todos
         }
         if(flag=='Active')
         {
-            // console.log(this.todos.filter(todo => !todo.completed))
             return this.todos.filter(todo => !todo.completed)
         }
         if(flag=='Completed')
         {
-            // console.log(this.todos.filter(todo => todo.completed))
             return this.todos.filter(todo => todo.completed)
         }
     }
@@ -81,7 +88,7 @@ class TodoService
 // Creating TodoService Instance/Object
 const todo = new TodoService()
 
-
+// Accessing Input Field
 let inputData = document.querySelector('#todoText')
 
 function completeAllAction()
@@ -98,7 +105,7 @@ inputData.addEventListener('keyup',event=>{
         if(title)
         {
             todo.createTodo(title)
-            event.target.value = ""
+            event.target.value = ""     // After todo added then input field will get blank
             renderTodos('All')
         }
     }
@@ -110,6 +117,20 @@ function makeItComplete(event,id)
     renderTodos('All')
 }
 
+
+function editTitle()
+{
+    document.querySelector('.todo-list-title').setAttribute('contenteditable','') // Setting the 'contenteditable' attribute to title tag
+}
+
+function takeNewTitle(event,id)
+{
+    let newTitle = event.target.innerText
+    todo.editTitle(id,newTitle)
+    renderTodos('All')
+}
+
+
 function deleteTodo(event,id)
 {
     todo.deleteTodo(id)
@@ -118,7 +139,7 @@ function deleteTodo(event,id)
 
 let flag = document.querySelector('.actions')
 flag.addEventListener('click',event=>{
-    event.stopPropagation()
+    event.stopPropagation()  // this stop bubbling of this event to parent elements, preventing any parent handlers from being notified of the event
     let flagAction = event.target.innerText
     renderTodos(flagAction)
 })
@@ -130,24 +151,26 @@ function clearCompleted(event)
     renderTodos('All')
 }
 
+let completeAllBtn = document.querySelector('#completeAll')
 
 function renderTodos(flag)
 {
-    let todos = todo.fetchTodo(flag)
-    let countUncompleted = todos.filter(todo=>todo.completed !=true)
+    let todos = todo.fetchTodo(flag) // Fetching all todos by flag value
+    let countUncompleted = todos.filter(todo=>todo.completed !=true) // Filtering  Uncompleted Todos
     const todoElements =  todos.map(todo=>{
         return `<li class="list-group-item">
             <div class="row">
                 <div class="todo-list-checkbox">
                     <input onchange="makeItComplete(event,${todo.id})" type="checkbox" ${todo.completed ? 'checked' : ''} />
                 </div>
-                <div class="todo-list-title">${todo.title}</div>
+                <div class="todo-list-title" ondblclick = "editTitle()" onblur ="takeNewTitle(event,${todo.id})">${todo.title}</div>
                 <div class="todo-list-btn">
                     <button onclick="deleteTodo(event,${todo.id})" class="deleteBtn" >delete</button>
                 </div>
                 </div>
         </li>`
     })
+    document.querySelector('#completeAll').style.visibility = 'visible'
     document.querySelector('#todo-list-container').innerHTML = todoElements.join(" ")
-    document.querySelector('.itemsCount').innerText = `${countUncompleted.length} items`
+    document.querySelector('.itemsCount').innerText = `${countUncompleted.length} items` // For showing how many uncompleted todos left
 }
